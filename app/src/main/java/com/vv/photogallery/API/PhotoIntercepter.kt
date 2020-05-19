278d3295234e4b04e60652c2bc45386c16f90064
vishalvn <65587068+vishalvn@users.noreply.github.com>
2020-05-19 12:02:38 +0530

    Create PhotoIntercepter.kt

---
+++ app/src/main/java/com/vv/photogallery/API/PhotoIntercepter.kt
@@ -0,0 +1,28 @@
+package com.example.photogallerykotlinversion.API
+
+import com.example.photogallerykotlinversion.Constants.API_KEY
+import okhttp3.HttpUrl
+import okhttp3.Interceptor
+import okhttp3.Request
+import okhttp3.Response
+
+class PhotoInterceptor : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest: Request = chain.request()
+
+        val newUrl:HttpUrl = originalRequest.url().newBuilder()
+            .addQueryParameter("api_key", API_KEY)
+            .addQueryParameter("format","json")
+            .addQueryParameter("nojsoncallback","1")
+            .addQueryParameter("extras","url_s")
+            .addQueryParameter("safesearch","1")
+            .build()
+
+        val newRequest:Request = originalRequest.newBuilder()
+            .url(newUrl)
+            .build()
+
+        return chain.proceed(newRequest)
+    }
+}
